047d60356e0e50f9fa171ad6bfe25f04e138bdc9
Steven Dean <sdlanglois1@gmail.com>
2020-05-05 17:41:11 -0400

    added constructors and classes for each type

--- app.js
+++ app.js
@@ -4,32 +4,96 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+// ​const OUTPUT_DIR = path.resolve(__dirname, "output")
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+function createManager () {
+    inquirer
+        .prompt ([
+            {
+                type: "input",
+                message: "What is the Manager's name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is the Manager's employee id?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What is the Manager's email?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What is the Manager's office number?",
+                name: "officeNumber"
+            },
+        ]).then(function(response){
+            console.log(response)
+        })
+}
+
+function createEngineer () {
+    inquirer
+        .prompt ([
+            {
+                type: "input",
+                message: "What is the Engineer's name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is the Engineer's employee id?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What is the Engineer's email?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What is the Engineer's Github name?",
+                name: "github"
+            },
+        ]).then(function(response){
+            console.log(response)
+        })
+}
+
+function createIntern () {
+    inquirer
+        .prompt ([
+            {
+                type: "input",
+                message: "What is the Intern's name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is the Intern's employee id?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What is the Intern's email?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What school does the intern attend?",
+                name: "school"
+            },
+        ])
+        .then(function(response){
+            console.log(response)
+        })
+}
+
+
+createManager()
+createEngineer()
+createIntern()
